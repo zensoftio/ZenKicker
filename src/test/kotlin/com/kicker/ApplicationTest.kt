3ffe88b6438d1670fa9125e03cda52a82584bea5
Yauheni <efim122@mail.ru>
2018-06-15 21:18:35 +0300

    Added configurations

---
+++ src/test/kotlin/com/kicker/ApplicationTest.kt
@@ -0,0 +1,16 @@
+package com.kicker
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
