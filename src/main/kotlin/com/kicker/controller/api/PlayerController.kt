dc7429f3abd2355a350087dbbf8cd76fcade5b31
Yauheni <efim122@mail.ru>
2018-06-16 17:53:14 +0300

    Fixed bug with CurrentPlayer

--- src/main/kotlin/com/kicker/controller/api/PlayerController.kt
+++ src/main/kotlin/com/kicker/controller/api/PlayerController.kt
@@ -3,9 +3,9 @@ package com.kicker.controller.api
 import com.kicker.annotation.CurrentPlayer
 import com.kicker.domain.PageRequest
 import com.kicker.domain.PageResponse
-import com.kicker.domain.model.CreatePlayerRequest
-import com.kicker.domain.model.PlayerDto
-import com.kicker.domain.model.UpdatePlayerRequest
+import com.kicker.domain.model.player.CreatePlayerRequest
+import com.kicker.domain.model.player.PlayerDto
+import com.kicker.domain.model.player.UpdatePlayerRequest
 import com.kicker.model.Player
 import com.kicker.service.PlayerService
 import org.springframework.web.bind.annotation.*
@@ -20,9 +20,7 @@ class PlayerController(
 ) {
 
     @GetMapping("/current")
-    fun getCurrent(@CurrentPlayer currentPlayer: Player): PlayerDto {
-        return PlayerDto(currentPlayer)
-    }
+    fun getCurrent(@CurrentPlayer currentPlayer: Player): PlayerDto = PlayerDto(currentPlayer)
 
     @GetMapping("/{userId}")
     fun get(@PathVariable userId: Long): PlayerDto = PlayerDto(service.get(userId))
--- src/main/kotlin/com/kicker/model/Player.kt
+++ src/main/kotlin/com/kicker/model/Player.kt
@@ -3,10 +3,7 @@ package com.kicker.model
 import com.kicker.model.base.BaseModel
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
-import javax.persistence.Column
-import javax.persistence.Entity
-import javax.persistence.OneToMany
-import javax.persistence.Table
+import javax.persistence.*
 
 /**
  * @author Yauheni Efimenko
@@ -30,7 +27,7 @@ class Player(
         @Column(name = "active", nullable = false)
         var active: Boolean = false,
 
-        @OneToMany(mappedBy = "player")
+        @OneToMany(mappedBy = "player", fetch = FetchType.EAGER)
         val awards: List<Award> = listOf()
 
 ) : BaseModel(), UserDetails {
