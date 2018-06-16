099b7b9ba1e07dc19965ecdee794c91ddaabc240
Yauheni <efim122@mail.ru>
2018-06-16 23:09:03 +0300

    Refactored configs

--- src/main/kotlin/com/kicker/config/FlywayConfig.kt
+++ src/main/kotlin/com/kicker/config/FlywayConfig.kt
@@ -4,17 +4,12 @@ import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.crypto.password.PasswordEncoder
 
 /**
  * @author Yauheni Efimenko
  */
 @Configuration
-class UtilConfig {
-
-    @Bean
-    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
+class FlywayConfig {
 
     @Profile("Put if need to clean")
     @Bean
--- src/main/kotlin/com/kicker/config/SecurityConfig.kt
+++ src/main/kotlin/com/kicker/config/SecurityConfig.kt
@@ -1,40 +1,54 @@
 package com.kicker.config
 
 import com.kicker.service.PlayerService
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 
 /**
  * @author Yauheni Efimenko
  */
 @Configuration
-class SecurityConfig(
-        private val userDetailsService: PlayerService,
-        private val passwordEncoder: PasswordEncoder
-) : WebSecurityConfigurerAdapter() {
+@EnableGlobalMethodSecurity
+class SecurityConfig : GlobalMethodSecurityConfiguration() {
 
-    override fun configure(auth: AuthenticationManagerBuilder) {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder)
-    }
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
 
-    override fun configure(http: HttpSecurity) {
-        http.csrf().disable()
 
-        http.authorizeRequests()
-                .antMatchers("/js/**").permitAll()
-                .antMatchers("/styles/**").permitAll()
-                .antMatchers("/images/**").permitAll()
+    @Configuration
+    class SecurityConfigurer(
+            private val userDetailsService: PlayerService,
+            private val passwordEncoder: PasswordEncoder
+    ) : WebSecurityConfigurerAdapter() {
 
-                .antMatchers("/**").authenticated()
+        override fun configure(auth: AuthenticationManagerBuilder) {
+            auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder)
+        }
 
-                .and()
+        override fun configure(http: HttpSecurity) {
+            http.csrf().disable()
 
-                .formLogin()
+            http.authorizeRequests()
+                    .antMatchers("/js/**").permitAll()
+                    .antMatchers("/styles/**").permitAll()
+                    .antMatchers("/images/**").permitAll()
+
+                    .antMatchers("/**").authenticated()
+
+                    .and()
+
+                    .formLogin()
 //                    .loginPage("/login")
 //                    .loginProcessingUrl("/login")
+        }
+
     }
 
 }
