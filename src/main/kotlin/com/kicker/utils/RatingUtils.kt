3448f1e68018d6b2d9678fba3eee46bb825051fc
Yauheni <efim122@mail.ru>
2018-06-22 00:27:28 +0300

    Added ratingUtils

---
+++ src/main/kotlin/com/kicker/utils/RatingUtils.kt
@@ -0,0 +1,19 @@
+package com.kicker.utils
+
+/**
+ * @author Yauheni Efimenko
+ */
+object RatingUtils {
+
+    private const val MAX_LOSING_PERCENTS = 6.0
+    private const val GOAL_DECREASE_COEFF = 1.0 / 4.0
+    private const val SKILL_CORRECTION_DEGREE = 0.2
+
+
+    fun getLosingPercents(goalsAgainst: Int, skillCorrection: Double): Double =
+            skillCorrection * (MAX_LOSING_PERCENTS - (goalsAgainst * GOAL_DECREASE_COEFF))
+
+    fun getSkillCorrection(losersTotalRating: Double, winnersTotalRating: Double): Double =
+            Math.pow(losersTotalRating / winnersTotalRating, SKILL_CORRECTION_DEGREE)
+
+}
