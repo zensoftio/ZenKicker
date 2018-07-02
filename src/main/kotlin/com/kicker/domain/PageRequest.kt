d6e55917fb9a8d66f6657dd145422ec76ca516bc
Yauheni Efimenko <zh.efimenko@gmail.com>
2018-07-02 15:28:49 +0300

    Fixed pageRequest

--- src/main/kotlin/com/kicker/domain/PageRequest.kt
+++ src/main/kotlin/com/kicker/domain/PageRequest.kt
@@ -1,6 +1,5 @@
 package com.kicker.domain
 
-import com.fasterxml.jackson.annotation.JsonIgnore
 import com.kicker.annotation.Contains
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
@@ -16,21 +15,17 @@ open class PageRequest(
         @field:Min(value = 1) @field:Max(100) private var limit: Int = 10,
         var sortDirection: Sort.Direction = Sort.Direction.ASC,
         var sortBy: String = "id",
-        @field:JsonIgnore
         val maySortBy: Map<String, String> = mapOf("id" to "id")
 ) : Pageable {
 
-    @JsonIgnore
     override fun getPageNumber(): Int = (offset / limit + 1)
 
     override fun hasPrevious(): Boolean = offset > 0
 
-    @JsonIgnore
     override fun getSort(): Sort = Sort(sortDirection, maySortBy[sortBy])
 
     override fun next(): Pageable = PageRequest(offset + limit, limit, sortDirection, sortBy, maySortBy)
 
-    @JsonIgnore
     override fun getPageSize(): Int = limit
 
     override fun getOffset(): Long = offset.toLong()
