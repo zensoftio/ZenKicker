a36bbe0bd9288a8a5f672df0edc3517bf52c78bf
evgeni <evgeni.efimenko@zensoft.io>
2018-10-01 12:22:07 +0300

    Improved creating and changing of password

--- src/main/kotlin/com/kicker/domain/model/player/CreatePlayerRequest.kt
+++ src/main/kotlin/com/kicker/domain/model/player/CreatePlayerRequest.kt
@@ -1,14 +1,11 @@
 package com.kicker.domain.model.player
 
-import com.kicker.annotation.FieldMatch
 import javax.validation.constraints.NotBlank
 
 /**
  * @author Yauheni Efimenko
  */
-@FieldMatch(first = "password", second = "retypePassword", message = "The password fields must match")
 data class CreatePlayerRequest(
         @field:NotBlank var username: String? = null,
-        @field:NotBlank var password: String? = null,
-        @field:NotBlank var retypePassword: String? = null
+        @field:NotBlank var password: String? = null
 )
