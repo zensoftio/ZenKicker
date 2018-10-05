d9f8bcdf78c5791590e3ec56a9356f4b06bbe157
Evgeni Efimenko <evgeni.efimenko@zensoft.io>
2018-10-05 10:01:41 +0300

    Added nginx, improved scripts

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@
 APP_IMAGE_NAME=kicker
 APP_SERVICE_NAME=kicker
 NETWORK_SERVICES_NAME=kicker-net
+
 POSTGRES_SERVICE_NAME=postgres_kicker
 POSTGRES_HOST=postgres
 POSTGRES_DB=kicker
@@ -11,9 +12,20 @@ POSTGRES_USER=postgres
 POSTGRES_PASSWORD=123
 OUTER_PORT=5440
 
+STATIC_FOLDERS_IMAGES=images
+VOLUME_IMAGES_NAME=images_volume
+
+NGINX_SERVICE_NAME=nginx
+NGINX_CONF_FILE_PATH=docker/nginx/nginx.conf
+
 
 # CLEAN ENVIRONMENT
-sh docker/clean.sh ${APP_IMAGE_NAME} ${APP_SERVICE_NAME} ${POSTGRES_SERVICE_NAME} ${NETWORK_SERVICES_NAME}
+sh docker/clean.sh ${APP_IMAGE_NAME} \
+                    ${APP_SERVICE_NAME} \
+                    ${POSTGRES_SERVICE_NAME} \
+                    ${NETWORK_SERVICES_NAME} \
+                    ${NGINX_SERVICE_NAME} \
+                    ${VOLUME_IMAGES_NAME}
 
 # BUILD APP
 ./gradlew clean assemble
@@ -25,20 +37,37 @@ docker build -t ${APP_IMAGE_NAME}:latest -f docker/Dockerfile .
 docker network create ${NETWORK_SERVICES_NAME}
 
 # RUN POSTGRES
-sh docker/postgres.sh ${POSTGRES_SERVICE_NAME} ${POSTGRES_DB} ${POSTGRES_USER} ${POSTGRES_PASSWORD} ${OUTER_PORT}
+sh docker/postgres.sh ${POSTGRES_SERVICE_NAME} \
+                        ${POSTGRES_DB} \
+                        ${POSTGRES_USER} \
+                        ${POSTGRES_PASSWORD} \
+                        ${OUTER_PORT}
 
 # CREATE ALIAS FOR HOSTNAME
-docker network connect --alias ${POSTGRES_HOST} ${NETWORK_SERVICES_NAME} ${POSTGRES_SERVICE_NAME}
+docker network connect --alias ${POSTGRES_HOST} \
+                                ${NETWORK_SERVICES_NAME} \
+                                ${POSTGRES_SERVICE_NAME}
 
-# RUN APP
-docker run \
-    -i \
-    --rm \
+# CREATE VOLUME BETWEEN CONTAINERS
+docker volume create --name ${VOLUME_IMAGES_NAME}
+
+# CREATE APP
+docker create \
     --name ${APP_SERVICE_NAME} \
     --network ${NETWORK_SERVICES_NAME} \
+    -v ${VOLUME_IMAGES_NAME}:/root/${STATIC_FOLDERS_IMAGES} \
     -e POSTGRES_HOST=${POSTGRES_HOST} \
     -e POSTGRES_DB=${POSTGRES_DB} \
     -e POSTGRES_USER=${POSTGRES_USER} \
     -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
+    -e STATIC_FOLDERS_IMAGES=${STATIC_FOLDERS_IMAGES} \
     -p 8080:8080 \
     ${APP_IMAGE_NAME}
+
+# RUN NGINX
+sh docker/nginx/nginx.sh ${VOLUME_IMAGES_NAME} \
+                            ${NGINX_SERVICE_NAME} \
+                            ${NGINX_CONF_FILE_PATH}
+
+# RUN APP
+docker start -i ${APP_SERVICE_NAME}
