3d3e7c60bc292ae71f2962a6794b08348cb2361e
Evgeni Efimenko <evgeni.efimenko@zensoft.io>
2018-10-05 17:26:02 +0300

    Improved exceptionAdvice

--- src/main/kotlin/com/kicker/controller/base/ExceptionRestControllerAdvice.kt
+++ src/main/kotlin/com/kicker/controller/base/ExceptionRestControllerAdvice.kt
@@ -4,12 +4,14 @@ import com.kicker.domain.exception.ErrorDto
 import com.kicker.domain.exception.ExceptionResponse
 import com.kicker.exception.controller.ControllerException
 import com.kicker.exception.service.ServiceException
-import org.springframework.http.HttpStatus.*
+import org.springframework.http.HttpStatus.BAD_REQUEST
+import org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR
 import org.springframework.validation.BindException
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.multipart.MaxUploadSizeExceededException
 import org.springframework.web.multipart.MultipartException
 import javax.validation.ConstraintViolationException
 
@@ -48,8 +50,13 @@ class ExceptionRestControllerAdvice {
 
     @ResponseStatus(code = BAD_REQUEST)
     @ExceptionHandler(MultipartException::class)
-    fun multipartExceptionHandler(exception: MultipartException): ExceptionResponse =
-            ExceptionResponse(BAD_REQUEST.value(), exception.message!!)
+    fun multipartExceptionHandler(exception: MultipartException): ExceptionResponse {
+        var message = exception.message!!
+        if (exception is MaxUploadSizeExceededException) {
+            message = "Maximum upload size exceeded"
+        }
+        return ExceptionResponse(BAD_REQUEST.value(), message)
+    }
 
     @ResponseStatus(code = BAD_REQUEST)
     @ExceptionHandler(ConstraintViolationException::class)
