d1a3a28ca8ba2edb2ff24efc4477187058d502e8
Evgeni Efimenko <evgeni.efimenko@zensoft.io>
2018-10-05 15:21:55 +0300

    Added icon validation

--- src/main/kotlin/com/kicker/controller/api/PlayerController.kt
+++ src/main/kotlin/com/kicker/controller/api/PlayerController.kt
@@ -3,9 +3,11 @@ package com.kicker.controller.api
 import com.kicker.annotation.CurrentPlayer
 import com.kicker.domain.PageResponse
 import com.kicker.domain.model.player.*
+import com.kicker.exception.controller.MultipartFileException
 import com.kicker.model.Player
 import com.kicker.service.GameService
 import com.kicker.service.PlayerService
+import org.springframework.http.MediaType.*
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 import javax.validation.Valid
@@ -61,12 +63,10 @@ class PlayerController(
     }
 
     @PutMapping("/icon")
-    fun updateUsername(@CurrentPlayer currentPlayer: Player, @RequestPart file: MultipartFile): PlayerDto {
-        if (file.isEmpty) {
-            throw Exception()
-        }
+    fun updateUsername(@CurrentPlayer currentPlayer: Player, @RequestPart("file") icon: MultipartFile): PlayerDto {
+        validateIcon(icon)
 
-        val player = service.updateIcon(currentPlayer.id, file)
+        val player = service.updateIcon(currentPlayer.id, icon)
         return PlayerDto(player, gameService.countByPlayer(player.id),
                 gameService.countFor10WeeksByPlayer(player.id))
     }
@@ -76,6 +76,17 @@ class PlayerController(
         val player = service.updatePassword(currentPlayer.id, request)
         return PlayerDto(player, gameService.countByPlayer(player.id),
                 gameService.countFor10WeeksByPlayer(player.id))
+    }
+
+    private fun validateIcon(icon: MultipartFile) {
+        if (icon.isEmpty) {
+            throw MultipartFileException("The uploaded file is empty")
+        }
+
+        if (icon.contentType != IMAGE_JPEG_VALUE && icon.contentType != IMAGE_PNG_VALUE
+                && icon.contentType != IMAGE_GIF_VALUE) {
+            throw MultipartFileException("Incorrect file type, ${icon.contentType}")
+        }
     }
 
 }
---
+++ src/main/kotlin/com/kicker/exception/controller/ControllerException.kt
@@ -0,0 +1,14 @@
+package com.kicker.exception.controller
+
+/**
+ * @author Yauheni Efimenko
+ */
+open class ControllerException : RuntimeException {
+    constructor(message: String?) : super(message)
+    constructor(message: String?, cause: Throwable?) : super(message, cause)
+}
+
+class MultipartFileException : ControllerException {
+    constructor(message: String?) : super(message)
+    constructor(message: String?, cause: Throwable?) : super(message, cause)
+}
