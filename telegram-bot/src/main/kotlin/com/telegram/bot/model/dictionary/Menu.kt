0fde3411a931860be0f164326b49fb2941d480cc
Evgeni Efimenko <evgeni.efimenko@zensoft.io>
2019-01-02 12:22:16 +0300

    Increased versions

--- telegram-bot/src/main/kotlin/com/telegram/bot/model/dictionary/Menu.kt
+++ telegram-bot/src/main/kotlin/com/telegram/bot/model/dictionary/Menu.kt
@@ -8,7 +8,8 @@ enum class Menu(
     HELP("/help", "The list of commands"),
     PLAYERS("/players", "The list of players"),
     PLAYER_STATS("/stats", "The stats of player. Example: /stats {player_id}"),
-    GAME_REGISTRATION("/registration", "Registration of game");
+    GAME_REGISTRATION("/registration", "Registration of game. Example: /registration {winner1_id} " +
+        "{winner2_id} {loser1_id} {loser2_id} {goals_against}");
 
     companion object {
         fun getItemMenu(command: String): Menu? = values().firstOrNull { it.command == command }
