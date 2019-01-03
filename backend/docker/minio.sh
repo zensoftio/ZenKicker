02596273be02d2750a9305d35616478b87058c11
Yauheni Efimenko <zh.efimenko@gmail.com>
2019-01-04 01:20:43 +0300

    Improved script of minio

--- backend/docker/minio.sh
+++ backend/docker/minio.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run -d -p 9000:9000 --name minio1 \
+docker run -d -p 9000:9000 --name minio \
   -e "MINIO_ACCESS_KEY=your_access_key" \
   -e "MINIO_SECRET_KEY=your_secret_key" \
   -v /home/${USER}/minio/data:/data \
