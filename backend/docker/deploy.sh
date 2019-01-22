9eb79c7aa89d717362e7f347a35b4e3464c94722
Evgeni Efimenko <evgeni.efimenko@zensoft.io>
2019-01-23 00:38:57 +0300

    Changed deploy.sh

--- backend/docker/deploy.sh
+++ backend/docker/deploy.sh
@@ -14,63 +14,52 @@ POSTGRES_DB=kicker
 POSTGRES_USER=postgres
 POSTGRES_PASSWORD=123
 OUTER_PORT=5440
-VC_POSTGRES_SERVICE_NAME=vc_${POSTGRES_SERVICE_NAME}
-VC_POSTGRES_SERVICE_EXIST=`docker ps -aq -f name=${VC_POSTGRES_SERVICE_NAME}`
-VOLUME_FOLDER=/var/lib/postgresql/data/pgdata
+POSTGRES_SERVICE_EXIST=`docker ps -aq -f name=${POSTGRES_SERVICE_NAME}`
+
+SERVER_HOST=http://new-kicker.zensoft.by
 
 ##################################
 ####### CLEAN ENVIRONMENT ########
 ##################################
 docker rm -vf ${APP_SERVICE_NAME}
-docker rm -vf ${POSTGRES_SERVICE_NAME}
 docker rmi -f ${APP_IMAGE_NAME}
-docker network rm ${NETWORK_SERVICES_NAME}
-# To uncomment if no needed to remove vc
-#docker rm -vf vc_${POSTGRES_SERVICE_NAME}
 
-# IF -c THAN EXIT SCRIPT
-if [ "$1" = "-c" ];
+# IF -r THAN EXIT SCRIPT
+if [[ "$1" = "-r" ]];
 then
+    docker rm -vf ${POSTGRES_SERVICE_NAME}
+    docker network rm ${NETWORK_SERVICES_NAME}
+    docker rm -vf vc_${POSTGRES_SERVICE_NAME}
     exit 1
 fi
 
-##################################
-######### CREATE NETWORK #########
-##################################
-docker network create ${NETWORK_SERVICES_NAME}
-
-##################################
-# CREATE VOLUME CONTAINER FOR DATABASE
-##################################
-if [ "${VC_POSTGRES_SERVICE_EXIST}" = "" ];
+if [[ "${POSTGRES_SERVICE_EXIST}" = "" ]];
 then
-    docker create \
-        --name ${VC_POSTGRES_SERVICE_NAME} \
-        -v ${VOLUME_FOLDER} \
-        busybox
+    ##################################
+    ######### CREATE NETWORK #########
+    ##################################
+    docker network create ${NETWORK_SERVICES_NAME}
+
+    ##################################
+    ######### RUN POSTGRES ###########
+    ##################################
+    docker run \
+        -d \
+        --name ${POSTGRES_SERVICE_NAME} \
+        --restart=always \
+        -e POSTGRES_DB=${POSTGRES_DB} \
+        -e POSTGRES_USER=${POSTGRES_USER} \
+        -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
+        -p ${OUTER_PORT}:5432 \
+        postgres:9.6.11
+
+    ##################################
+    ### CREATE ALIAS FOR HOSTNAME ####
+    ##################################
+    docker network connect --alias ${POSTGRES_HOST} \
+                                    ${NETWORK_SERVICES_NAME} \
+                                    ${POSTGRES_SERVICE_NAME}
 fi
-
-##################################
-######### RUN POSTGRES ###########
-##################################
-docker run \
-    -d \
-    --name ${POSTGRES_SERVICE_NAME} \
-    --restart=always \
-    --volumes-from ${VC_POSTGRES_SERVICE_NAME} \
-    -e PGDATA=${VOLUME_FOLDER} \
-    -e POSTGRES_DB=${POSTGRES_DB} \
-    -e POSTGRES_USER=${POSTGRES_USER} \
-    -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
-    -p ${OUTER_PORT}:5432 \
-    postgres:9.6.11
-
-##################################
-### CREATE ALIAS FOR HOSTNAME ####
-##################################
-docker network connect --alias ${POSTGRES_HOST} \
-                                ${NETWORK_SERVICES_NAME} \
-                                ${POSTGRES_SERVICE_NAME}
 
 ##################################
 ############ CREATE IMAGE ########
@@ -88,5 +77,6 @@ docker run \
     -e POSTGRES_DB=${POSTGRES_DB} \
     -e POSTGRES_USER=${POSTGRES_USER} \
     -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
+    -e SERVER_HOST=${SERVER_HOST} \
     -p 8080:8080 \
     ${APP_IMAGE_NAME}
--- backend/docker/minio.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker run -d -p 9000:9000 --name minio \
-  -e "MINIO_ACCESS_KEY=your_access_key" \
-  -e "MINIO_SECRET_KEY=your_secret_key" \
-  -v /home/${USER}/minio/data:/data \
-  -v /home/${USER}/minio/config:/root/.minio \
-  minio/minio server /data
