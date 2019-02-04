1ad1b84e2588dbd149443fabaf9ad1e6a5e16196
Evgeni Efimenko <evgeni.efimenko@zensoft.io>
2019-02-05 00:02:29 +0300

    Removed telegram-bot

--- telegram-bot/src/main/kotlin/com/telegram/bot/domain/GameRegistrationRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.telegram.bot.domain
-
-data class GameRegistrationRequest(
-        val winner1Id: Long,
-        val winner2Id: Long,
-        val loser1Id: Long,
-        val loser2Id: Long,
-        val losersGoals: Int
-)
